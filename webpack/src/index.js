33868ab5639c123ab8423f64905372e0ef7b1fa8
wuying <wuy_123@126.com>
2018-08-07 11:46:29 +0800

    webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,4 +2,4 @@
  * Created by apple on 18/8/7.
  */
 var oDiv1 = document.getElementById('div1');
-oDiv1.innerHTML = 'hello world111sss';
+oDiv1.innerHTML = 'hello world111sssss';
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');//webpack自带path模块
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode:"development",
     entry:{
@@ -8,10 +10,23 @@ module.exports = {
         path:path.resolve(__dirname,'dist'),
         filename:'[name].js'
     },
+    module:{},
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            minify:{
+                removeAttributeQuotes:true
+            },
+            hash:true,
+            template:'./src/index.html'
+        })
+    ],
     devServer:{
         contentBase:path.resolve(__dirname,'dist'),
         host:'127.0.0.1',
         port:'8080',
-        compress:true
+        compress:true,
+        hot:true,
+        open:true
     }
 }
