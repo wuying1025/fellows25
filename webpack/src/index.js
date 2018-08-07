8bf684b0e993ed62836f9c289a618ac6b0c878a5
wuying <wuy_123@126.com>
2018-08-07 14:02:32 +0800

    webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,5 +1,7 @@
 /**
  * Created by apple on 18/8/7.
  */
+import css from './css/style.css';
+
 var oDiv1 = document.getElementById('div1');
 oDiv1.innerHTML = 'hello world111sssss';
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,25 +1,50 @@
 const path = require('path');//webpack自带path模块
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     mode:"development",
     entry:{
-        'index': "./src/index.js"
+        'index': "./src/index.js",
+        'index2':"./src/index2.js"
     },
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'[name].js'
     },
-    module:{},
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                // use:['style-loader','css-loader']
+                use:ExtractTextWebpackPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
     plugins:[
         new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
+            filename:'index.html',
+            chunks:['index'],
             minify:{
                 removeAttributeQuotes:true
             },
             hash:true,
             template:'./src/index.html'
-        })
+        }),
+        // new HtmlWebpackPlugin({
+        //     filename:'index2.html',
+        //     chunks:['index2'],
+        //     minify:{
+        //         removeAttributeQuotes:true
+        //     },
+        //     hash:true,
+        //     template:'./src/index2.html'
+        // }),
+        new ExtractTextWebpackPlugin("./css/style.css")
     ],
     devServer:{
         contentBase:path.resolve(__dirname,'dist'),
