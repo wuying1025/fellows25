c9b86e95c76e11c7d111595884791ab92c70b1bf
wuying <wuy_123@126.com>
2018-08-08 11:32:21 +0800

    webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -6,3 +6,9 @@ import scss from './scss/style.scss';
 
 var oDiv1 = document.getElementById('div1');
 oDiv1.innerHTML = 'hello world111sssss';
+
+let a = 10;
+let b = (c)=>{
+    console.log(c);
+}
+b(a);
