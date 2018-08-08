da53e85fa0f5509f90186a5f110e5f9753fb5ace
wuying <wuy_123@126.com>
2018-08-08 11:06:01 +0800

    webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,6 +2,7 @@
  * Created by apple on 18/8/7.
  */
 import css from './css/style.css';
+import scss from './scss/style.scss';
 
 var oDiv1 = document.getElementById('div1');
 oDiv1.innerHTML = 'hello world111sssss';
