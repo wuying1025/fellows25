d9844b6a931248f2412e6fb35032499a2cf67f15
wuying <wuy_123@126.com>
2018-08-16 10:06:33 +0800

    vue

---
+++ vueapp/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <CommonHeader></CommonHeader>
+      <router-view/>
+    <CommonFooter></CommonFooter>
+  </div>
+</template>
+<script>
+  import CommonHeader from '@/components/CHeader.vue';
+  import CommonFooter from '@/components/CommonFooter.vue';
+  export default {
+
+      components:{
+          CommonHeader,
+          CommonFooter
+      }
+
+
+
+
+
+
+  }
+
+
+
+</script>
+<style lang="scss">
+
+</style>
---
+++ vueapp/src/components/CommonFooter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="footer">
+
+    </div>
+</template>
+<script>
+    
+</script>
+<style lang="scss">
+    .footer {
+        height: 1rem;
+        width: 100%;
+        background: rgb(33, 150, 243);
+        color: #fff;
+        position: fixed;
+        bottom: 0;
+        line-height: 1rem;
+    }
+</style>
