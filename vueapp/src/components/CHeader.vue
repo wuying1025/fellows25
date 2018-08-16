d6a5823551805f5b3dfe9270ee96c26ce19564e1
wuying <wuy_123@126.com>
2018-08-16 10:05:52 +0800

    vue

---
+++ vueapp/src/components/CHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header">
+        <span class="header-home">首页</span>
+        <h1 class="header-title">movie</h1>
+    </div>
+</template>
+<script>
+    
+</script>
+<style lang="scss">
+    .header{
+        height: 1rem;
+        width: 100%;
+        background:rgb(33, 150, 243);
+        color: #fff;
+        position:fixed;
+        top:0;
+        line-height: 1rem;
+        &-home{
+             font-size: 16px;
+
+         }
+        &-title{
+            position: absolute;
+            top:50%;
+            left:50%;
+            transform: translate(-50%,-50%);
+        }
+    }
+</style>
