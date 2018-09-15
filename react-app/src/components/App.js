c87311c848d9436d58ffa5a4a5cca7c1748411da
dongxiuling <dxl_web@163.com>
2018-09-15 16:17:07 +0800

    redux

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import store from './../store';
+
+//action 
+const add = () => {
+	console.log('---action');
+	return {
+		type:'ADD'
+	}
+}
+
 
 class App extends Component {
-  render() {
-    return (
-		<div>
-			哈哈
-		</div>
-    );
-  }
+	addHandle(){
+		//触发action
+		store.dispatch(add());
+	}
+	render() {
+		return (
+			<div>
+				<h1>标题</h1>
+				<button onClick = {this.addHandle.bind(this)}>click</button>
+			</div>
+		);
+	}
 }
 
 export default App;
