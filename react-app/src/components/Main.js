a6a37b25f7429a6038d535ca987fa28938cea5a0
dongxiuling <dxl_web@163.com>
2018-09-16 16:09:24 +0800

    router

--- react-app/src/components/Main.js
+++ react-app/src/components/Main.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link ,Switch} from 'react-router-dom';
 import PageA from './PageA';
 import PageB from './PageB';
-
+import Home from './Home';
 class Main extends Component {
     render() {
         return (
@@ -10,10 +10,18 @@ class Main extends Component {
                 main主页面
                 <Router>
                     <div>
-                        <Link to="/a">跳转A</Link>
+                        <Link to="/">首页</Link>
+                        ---
+                        <Link to="/a/5">跳转A</Link>
                         ----
                         <Link to="/b">跳转B</Link>
-                        <Route path="/a" component={PageA}></Route>
+                        {/* <Switch>
+                            <Route path="/a" component={PageA}></Route>
+                            <Route path="/b" component={PageB}></Route>
+                            <Route path="/" component={Home}></Route>
+                        </Switch> */}
+                        <Route exact path="/" component={Home}></Route>
+                        <Route path="/a/:id" component={PageA}></Route>
                         <Route path="/b" component={PageB}></Route>
                     </div>
                 </Router>
@@ -21,5 +29,4 @@ class Main extends Component {
         );
     }
 }
-
 export default Main;
--- react-app/src/components/PageA.js
+++ react-app/src/components/PageA.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
-class PageA extends Component {
-    render() {
-        return (
-            <div>
-                A页面
-            </div>
-        );
-    }
+// class PageA extends Component {
+//     constructor(props) {
+//         super(props);
+//         console.log(props.match.params.id)
+//     }
+    
+//     render() {
+//         return (
+//             <div>
+//                 A页面
+
+//                 {this.props.match.params.id}
+//             </div>
+//         );
+//     }
+// }
+
+const PageA = ({match}) =>{
+    return (
+        <div> A页面
+            {match.params.id}
+        </div>
+    )
 }
 
 export default PageA;
