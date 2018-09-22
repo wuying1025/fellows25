df7321b06bd3b489c643f51b9eea1bf060bb6162
dongxiuling <dxl_web@163.com>
2018-09-22 12:49:26 +0800

    美团

--- meituan/src/components/App.js
+++ meituan/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import { Button } from 'antd-mobile';
+import './../asset/css/test.scss';
 export default class App extends Component {
   render() {
     return (
       <div>
           app
+          <Button>default</Button>
       </div>
     )
   }
