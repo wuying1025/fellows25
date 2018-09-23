c53f995e949671b3bad3b25b5e798f83b06073a2
dongxiuling <dxl_web@163.com>
2018-09-23 17:00:16 +0800

    meituan

--- meituan/src/components/Detail.js
+++ meituan/src/components/Detail.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
+import 'whatwg-fetch';
+import DImg from './DImg';
+
 
 export default class Detail extends Component {
-  constructor(props) {
-    super(props);
-    
-  }
-  
-  render() {
-    return (
-      <div>
-        {/* detail {
-          this.props.match.params.id
-        } */}
-          
-      </div>
-    )
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			row:{},
+			results:{}
+		}
+		
+	}
+	componentWillMount(){
+		fetch('http://www.xiechenxi.cn/welcome/detail/'+this.props.match.params.id)
+			.then((response)=>{
+				return response.json();
+			})
+			.then((data)=>{
+				this.setState({
+					row:data.row,
+					results:data.results
+				})
+				console.log(data);
+			})
+	}
+	render() {
+		return (
+			<div>
+				<DImg row={this.state.row}></DImg>
+				
+
+
+			</div>
+		)
+	}
 }
