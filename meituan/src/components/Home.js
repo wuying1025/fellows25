c06ed01cb194a5f2da57051da196f829867d898d
dongxiuling <dxl_web@163.com>
2018-09-23 14:22:53 +0800

    meituan

--- meituan/src/components/Home.js
+++ meituan/src/components/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { NavBar, Icon, SearchBar, Grid} from 'antd-mobile';
+import { NavBar, Icon, SearchBar, Grid, Flex, WhiteSpace} from 'antd-mobile';
+import './../asset/css/reset.css';
 import './../asset/css/home.css';
-
+import './../asset/js/rem.js';
+import Container from './Container';
 
 export default class Home extends Component {
     constructor(props) {
@@ -81,12 +83,29 @@ export default class Home extends Component {
                 >
                 <SearchBar placeholder="输入搜索内容" maxLength={8} />
             </NavBar>
-            <Grid data={this.state.data} isCarousel columnNum={5} />
-
-            
-
-            
-            
+            <Grid itemStyle={{height:'100px'}} data={this.state.data} isCarousel columnNum={5} hasLine={false} />
+            <WhiteSpace size="lg"></WhiteSpace>
+            <Flex>
+                <Flex.Item>
+                    <h3 className="active-title">我们约会吧</h3>
+                    <p className="active-text">恋人家人好朋友</p>
+                    <img className="active-img" src={require("./../asset/img/activity1.png")}></img>
+                </Flex.Item>
+                <Flex.Item>
+                <h3 className="active-title">我们约会吧</h3>
+                    <p className="active-text">恋人家人好朋友</p>
+                    <img className="active-img" src={require("./../asset/img/activity1.png")}></img>
+                </Flex.Item>
+                <Flex.Item>
+                <h3 className="active-title">我们约会吧</h3>
+                    <p className="active-text">恋人家人好朋友</p>
+                    <img className="active-img" src={require("./../asset/img/activity1.png")}></img>
+                </Flex.Item>
+            </Flex>
+            <WhiteSpace size="lg"></WhiteSpace>
+            <Container title="猜你喜欢" >
+                <h2>这是我的内容</h2>
+            </Container>
         </div>
     )
   }
