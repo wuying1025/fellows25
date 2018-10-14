944f5f28f90474aa1be250c664c8ec21c58f0569
wuying <wuy_123@126.com>
2018-10-14 10:35:33 +0800

    php

--- myPhp/server.php
+++ myPhp/server.php
@@ -6,7 +6,22 @@
  * Time: 上午9:21
  */
 
-$name = $_GET['username'];
-$p = $_GET['pwd'];
+//$name = $_GET['username'];
+//$p = $_GET['pwd'];
 
-echo "您的用户名是:".$name."密码是:".$p;
+$name = $_POST['username'];
+$p = $_POST['pwd'];
+//$s = $_POST['sex'];
+//$h = $_POST['hobby'];
+//
+//var_dump($h);
+
+if($name=='admin' && $p == '123'){
+    echo '恭喜成功登录';
+}else{
+    echo '用户名或者密码错误';
+}
+
+//echo "您的用户名是:".$name."密码是:".$p,$s;
+
+//get  post   put  delete  head
