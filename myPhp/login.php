11c738d431ad53202092c1ffc442cae3792fd014
wuying <wuy_123@126.com>
2018-10-14 09:27:48 +0800

    php

---
+++ myPhp/login.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="server.php">
+        用户名:<input type="text" name="username"><br/>
+        密码: <input type="password" name="pwd"><br/>
+        <input type="submit" value="登录">
+    </form>
+
+
+
+</body>
+</html>
---
+++ myPhp/server.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 18/10/14
+ * Time: 上午9:21
+ */
+
+$name = $_GET['username'];
+$p = $_GET['pwd'];
+
+echo "您的用户名是:".$name."密码是:".$p;
